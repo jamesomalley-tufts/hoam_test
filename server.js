6d3a5a4a2066b6df473dea3800c42f0d3d6d8430
jamesomalley-tufts <james.o_malley@tufts.edu>
2025-05-01 23:41:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -50,11 +50,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 const FILE_API_URL = "https://67a08egpff.execute-api.us-east-2.amazonaws.com/test/upload";
 const FILE_API_KEY = "N0I50xLGdz9LmOpHw32th8aN0nLnhhxW1vKLG5Q5";
 
-// Middleware
-app.use(cors());
-app.use(express.static('public')); 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 
 // Pages
 app.get('/', (req, res) => {
@@ -205,6 +201,12 @@ io.on('connection', (socket) => {
     console.log('user disconnected');
   })
 });
+
+// Middleware
+app.use(cors());
+app.use(express.static('public')); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Start server
 const PORT = process.env.PORT || 3000;
